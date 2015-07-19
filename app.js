8f7992c35097be9d23435a4f22d57b0a77d8c3e0
Alda <alda_liu@hotmail.com>
2015-07-19 18:00:47 -0400

    fixed css for pages

--- app.js
+++ app.js
@@ -22,12 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
+app.use('/', routes);
 app.use('/users', users);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/html/login.html');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+	var dir = path.resolve('./html/login.html');
+	res.sendFile(dir);
 });
 
 module.exports = router;

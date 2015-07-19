da0bd0d06d29809d3e11917b38a422c4a5bdfa70
Alda <alda_liu@hotmail.com>
2015-07-19 18:45:57 -0400

    added registration page

--- routes/login.js
+++ routes/login.js
@@ -8,5 +8,11 @@ router.get('/', function(req, res){
 	var dir = path.resolve('./html/login.html');
 	res.sendFile(dir);
 });
+router.get('/register', function(req, res){
+	var dir = path.resolve('./html/register.html');
+	res.sendFile(dir);
+});
+// var asd = document.getElementById("submitButton");
+// console.log(asd);
 
 module.exports = router;

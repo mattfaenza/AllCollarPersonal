8c075195a085f7bf4a7dd6aa882ab076538f2866
aldaliu <alda.liu@mail.utoronto.ca>
2015-07-28 15:27:26 -0400

    fixed some syntax errors with dashboard.js and search.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var path = require('path');
 
 
-router.get('/', function(req,res)){
+router.get('/', function(req,res){
 	var dir = path.resolve('./html/dashboard.html');
 	res.sendFile(dir)
-}
+});
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req,res)){
+router.get('/', function(req,res){
 	res.send("User's profile");
-}
+});
 
 module.exports = router;

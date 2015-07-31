245f22f19589f04792e1b895e25811c78d061332
aldaliu <alda.liu@mail.utoronto.ca>
2015-07-31 01:10:47 -0400

    fixed conflict in dashboard.js from previous merge

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,4 @@
 var express = require('express');
-<<<<<<< HEAD
-var router = express.Router();
-var path = require('path');
-
-
-router.get('/', function(req,res){
-=======
 var app = express();
 var router = express.Router();
 var path = require('path');
@@ -13,17 +6,12 @@ var mongoose = require('mongoose');
 
 var dashboard = require('../models/dashboard');
 
-
 /* GET home page. */
 router.get('/', function(req, res){
->>>>>>> Mongoose-Attempt2
 	var dir = path.resolve('./html/dashboard.html');
 	res.sendFile(dir);
 });
 
-<<<<<<< HEAD
-module.exports = router;
-=======
 router.get('/profile', function(req, res){
 	var dir = path.resolve('./html/profile.html');
 	res.sendFile(dir);
@@ -48,4 +36,3 @@ router.get('/jobs', function(req, res){
 });
 
 module.exports = router;
->>>>>>> Mongoose-Attempt2

7d7d65303d7bfe3a1d3c459d497353e4d7ab070d
aldaliu <alda.liu@mail.utoronto.ca>
2015-07-31 16:57:13 -0400

    Implemented registration

--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,8 @@ var app = express();
 var router = express.Router();
 var path = require('path');
 
+var User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res){
 	var dir = path.resolve('./html/register.html');
@@ -11,6 +13,23 @@ router.get('/', function(req, res){
 router.get('/login', function(req, res){
 	var dir = path.resolve('./html/login.html');
 	res.sendFile(dir);
+});
+
+router.post('/', function(req, res){
+	var user = new User({	
+		username: req.body.user,
+		first: req.body.fname,
+		last: req.body.lname,
+		email: req.body.email
+	});
+
+	user.save(function(err) {
+ 		if (err) throw err;
+
+	 	console.log(user.username +' saved successfully!');
+	});
+
+	res.redirect('/');
 });
 // var asd = document.getElementById("submitButton");
 // console.log(asd);

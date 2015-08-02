2781b500c44d4e0aa03a318c537a61313e983d6d
Matthew Faenza <matthew.faenza@mail.utoronto.ca>
2015-08-02 01:16:42 -0400

    more syntax stuffs

--- config/passport.js
+++ config/passport.js
@@ -74,7 +74,7 @@ module.exports = function(passport) {
 
         }
 
-    });
+    )};
 	
 	passport.use('local-login', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with username

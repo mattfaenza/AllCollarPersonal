22e27db8a573c7145b75a3bcd56dc707f58eba95
Matthew Faenza <matthew.faenza@mail.utoronto.ca>
2015-08-02 00:24:05 -0400

    More syntax fiddling

--- config/passport.js
+++ config/passport.js
@@ -34,8 +34,9 @@ module.exports = function(passport) {
         // we are checking to see if the user trying to login already exists
         User.findOne({ 'local.username' :  username }, function(err, user) {
             // if there are any errors, return the error
-            if (err)
+            if (err) {
                 return done(err);
+				};
 
             // check to see if theres already a user with that username
             if (user) {
@@ -45,12 +46,13 @@ module.exports = function(passport) {
 				// check to see if theres already a user with that email
 				User.findone({ 'local.email' :  email }, function(err, user) {
 				// if there are any errors, return the error
-					if (err)
+					if (err) {
 						return done(err);
+						};
 					if (user) {
 						return done(null, false, req.flash('registerMessage', 'That username is already taken.'));
-						}
-					}
+						};
+					});
 				// if there is no user with that username or email
                 // create the user
                 var newUser = new User();

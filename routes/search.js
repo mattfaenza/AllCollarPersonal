782202064f59f5c851a23b1da7062cf65999d90a
aldaliu <alda.liu@mail.utoronto.ca>
2015-08-03 06:29:47 -0400

    Starting to implement search

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var mongoose = require('mongoose');
 
+var job = require('../models/job');
 
-router.get('/', function(req,res){
-	res.send("search page");
+var isAuthenticated = require('../config/authentication');
+
+router.use(isAuthenticated);
+
+router.get('/', isAuthenticated, function(req,res){
+	job.find({}, function(err, jobs){
+		if (err)
+			throw err;
+		res.render('search', { "jobs": jobs,
+								"user": req.user});
+	});
+	
 });
 
 module.exports = router;

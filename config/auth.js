ec46c0126cc04cc8a6804e2378c796b882025288
Matthew Faenza <matthew.faenza@mail.utoronto.ca>
2015-08-03 01:30:09 -0400

    Revert "adding module to see if anything changed"
    This reverts commit 1e1b3fed3b307d633a03e2c2a6413cebc2b46db4.

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,18 @@ module.exports = {
         'clientID'      : '404711866388396', // your App ID
         'clientSecret'  : '69bce50d350dda53a516eb735a06110e', // your App Secret
         'callbackURL'   : 'https://allcollar.herokuapp.com/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
-	
+
 };

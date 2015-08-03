0dae099f3cd785ce16ba53eda355acdf131bc160
aldaliu <alda.liu@mail.utoronto.ca>
2015-08-03 15:52:55 -0400

    Implemented searching

--- routes/search.js
+++ routes/search.js
@@ -9,14 +9,29 @@ var isAuthenticated = require('../config/authentication');
 
 router.use(isAuthenticated);
 
+router.post('/', isAuthenticated, function(req,res, next){
+	res.redirect('/search');
+	next();
+});
+//var search = "software developer";
 router.get('/', isAuthenticated, function(req,res){
-	job.find({}, function(err, jobs){
+	var regex = new RegExp(req.query["term"], 'i');
+	console.log(regex);
+	// if (search == ""){
+	// 	job.find({}, function(err, jobs){
+	// 		if (err)
+	// 			throw err;
+	// 		res.render('search', { "jobs": jobs,
+	// 								"user": req.user});
+	// 	});
+	// }else{
+	job.find({'title' : regex}, function(err, jobs){
+		console.log(jobs);
 		if (err)
 			throw err;
 		res.render('search', { "jobs": jobs,
 								"user": req.user});
 	});
-	
-});
 
+});
 module.exports = router;
